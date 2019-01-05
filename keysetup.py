266e2342b39c9a91203de7b9271fa7654f7e554d
Grace <contact@jhgrace.com>
2019-01-06 12:57:00 +1300

    added comments everywhere
    also changed some stuff up; mostly added the macro switching to an actual function instead of using global variables.  Oh, and I got rid of some extra code that wasn't needed with the kbd.press events

--- keysetup.py
+++ keysetup.py
@@ -1,14 +1,11 @@
-from board import A1, A2, SCL, SDA
+from board import SCL, SDA
 from busio import I2C
 from adafruit_mcp230xx import MCP23017
-from analogio import AnalogIn
 
 i2c = I2C(SCL, SDA)
+mcp1 = MCP23017(i2c, address=0x20)  # I can add up to 8 of these if I remember correctly, each for 16 keys each.  I could matrix them to get more, but that adds more delay and I'm running in to a speed issue as-is
 
-mcp1 = MCP23017(i2c, address=0x20)
-
-
-kbd_pins = [
+kbd_switches = [
     mcp1.get_pin(8),
     mcp1.get_pin(9),
     mcp1.get_pin(10),
--- main.py
+++ main.py
@@ -1,55 +1,52 @@
 import digitalio
-
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard import Keyboard
 
-from keysetup import kbd_pins
+from keysetup import kbd_switches  # imports all the hardware switch locations from the PCB
 
-kbd = Keyboard()
-kbd_keys = []
-toggles = []
-queuepress = []
-queuerelease = []
-prev = []
+kbd = Keyboard()  # This is the virtual keyboard device
+kbd_keys = []  # This is the array of all the virtual keys that the hardware keys will 'press' to the operating system
+toggles = []  # An array to check whether the state of the switch has changed each tick to avoid pressing a key many times unintentionally
+queue_press = []  # I'm looping through and adding all the changed key states and sending them in bulk after they've been checked
+queue_release = []	# And these two queue arrays are those key state changes
 
-macros = ["macro1.py", "macro2.py", "macro3.py"]
-which_macro = 0
-macro = macros[which_macro]
+def definemacro(index):	 # I'm not really using this yet, but hopefully one day I'll be able to store many different keymaps and swap them out at will
+	macros = ["macro1.py", "macro2.py", "macro3.py"]  # each keymap is stored in a different file to make it readable and extensible
+	which_macro = index
+	macro = macros[which_macro]
+	file = open(macro, "r")
+	macro_keys = []
+	for line in file:  # Each switch stores the buttons it will press in its own line, and each switch can press multiple keys at a time
+		macro_keys.append(eval(line))
+	return macro_keys
+		
+kbd_keys = definemacro(0)
 
-for pin in kbd_pins:
-	pin.direction = digitalio.Direction.INPUT
-	pin.pull = digitalio.Pull.UP
-	toggles.append(False)
-
-file = open(macro, "r")
-for line in file:
-	kbd_keys.append(eval(line))
+for switch in kbd_switches:	 # Do the hardware setup so the Feather knows what to do
+	switch.direction = digitalio.Direction.INPUT
+	switch.pull = digitalio.Pull.UP
+	toggles.append(False)  # each key starts in the state of being 'un-pressed'
 
 print("Waiting for Keypresses")
 
 while True:
-	for key in kbd_pins:
-		key_index = kbd_pins.index(key)
+	for key in kbd_switches:
+		key_index = kbd_switches.index(key)
 		
-		if not key.value:  # pressed?
-			if not toggles[key_index]:
-				toggles[key_index] = True
-				key_press = kbd_keys[key_index]
-				for press in key_press:
-					queuepress.append(press)
-		else:
-			if toggles[key_index]:
-				toggles[key_index] = False
-				key_release = kbd_keys[key_index]
-				for release in key_release:
-					queuerelease.append(release)
+		if not key.value:  # was a key pressed?
+			if not toggles[key_index]:  # Was the key previously un-pressed?  Only continue if the key's state has *changed*
+				toggles[key_index] = True  # Change that key's state,
+				queue_press.append(kbd_keys[key_index])	 # and add the keypresses to the queue to be pressed
+		else:  # Yeah no shit, Sherlock; if the key is not pressed, it must be released!
+			if toggles[key_index]:  # Was the key previously pressed?  Only continue if the key's state has *changed*
+				toggles[key_index] = False  # Change that key's state,
+				queue_release.append(kbd_keys[key_index])  # and add the keypresses to the queue to be released
 
-	queuepress = queuepress[:5]
-	for release in queuerelease:
-		kbd.release(release)					
-	for press in queuepress:
-		kbd.press(press)
+	for release in queue_release:  # release first to *try* and avoid that 6-keypress limit that keyboards have.
+		kbd.release(*release)					
+	for press in queue_press:
+		kbd.press(*press)
 
 	
-	queuepress = []
-	queuerelease = []
+	queue_press = []  # clear the arrays and start again
+	queue_release = []
