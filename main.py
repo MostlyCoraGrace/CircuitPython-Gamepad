4dd6112880e3ab306fa044f4b469edb85c622e70
Grace <contact@jhgrace.com>
2019-01-07 20:09:54 +1300

    still not sure why its not working, but at least its cleaner now?

--- main.py
+++ main.py
@@ -9,14 +9,11 @@ mcp1 = MCP23017(I2C(SCL, SDA), address=0x20)  # I can add up to 8 of these if I 
 kbd = Keyboard()  # This is the virtual keyboard device
 kbd_keys = []  # This is the array of all the virtual keys that the hardware keys will 'press' to the operating system
 toggles = []  # An array to check whether the state of the switch has changed each tick to avoid pressing a key many times unintentionally
-queue_press = set()  # I'm looping through and adding all the changed key states and sending them in bulk after they've been checked
-queue_release = set()   # And these two queue arrays are those key state changes
-kbd_switches = []
+kbd_switches = []  # the array of physical switch connections
 
-for i in range(15):
+for i in range(16):  # Do the hardware setup so the Feather knows what to do
     kbd_switches.append(mcp1.get_pin(i))
-
-for switch in kbd_switches:  # Do the hardware setup so the Feather knows what to do
+for switch in kbd_switches:
     switch.direction = digitalio.Direction.INPUT
     switch.pull = digitalio.Pull.UP
     toggles.append(False)  # each key starts in the state of being 'un-pressed'
@@ -32,34 +29,22 @@ def definemacro(index):  # I'm not really using this yet, but hopefully one day 
     return macro_keys
         
 kbd_keys = definemacro(0)
-print(kbd_keys)
 
 print("Waiting for Keypresses")
 
 while True:
-    buttons = list("{:015b}".format(mcp1.gpio))
+    buttons = []
+    for i in range(16):
+        buttons.append(bool(mcp1.gpio & 1 << i))  # each MCP23017 can output the state of all the GPIO keys at once, and this will make an array containing a bool for the state of each key.  MUCH faster than checking each key individually because I2C is relatively slow.
     for button in buttons:
         index = buttons.index(button)
-        buttons[index] = int(buttons[index])
-        if not buttons[index]:  # was a key pressed?
+        if not button:  # was a key pressed?
             if not toggles[index]:  # Was the key previously un-pressed?  Only continue if the key's state has *changed*
                 toggles[index] = True  # Change that key's state,
                 print("Key pressed")
-                keypress = kbd_keys[index]
-                for key in keypress:
-                    queue_press.add(key)     # and add the keypresses to the queue to be pressed
+                kbd.press(*kbd_keys[index])  # Press the key
         else:  # Yeah no shit, Sherlock; if the key is not pressed, it must be released!
             if toggles[index]:  # Was the key previously pressed?  Only continue if the key's state has *changed*
                 toggles[index] = False  # Change that key's state,
                 print("key released")
-                keypress = kbd_keys[index]
-                for k in keypress:
-                    queue_release.add(k)  # and add the keypresses to the queue to be released
-        
-        for release in queue_release:  # release first to *try* and avoid that 6-keypress limit that keyboards have.
-            kbd.release(release)                   
-        for press in queue_press:
-            kbd.press(press)
-
-    queue_press = set()  # clear the arrays and start again
-    queue_release = set()
+                kbd.release(*kbd_keys[index])  # Release the key
